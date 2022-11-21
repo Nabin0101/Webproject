ce0abd1cb7ef1b8f3514faeef263637c5ffaa1a3
Nabin Dahal <101242280+Nabin0101@users.noreply.github.com>
2022-11-21 18:38:36 +0545

    Add files via upload

---
+++ webproject/connection.php
@@ -0,0 +1,13 @@
+<?php
+$server ="localhost";
+$user ="root";
+$password= "";
+$database ="login_register";
+
+$conn =mysqli_connect($server,$user,$password,$database);
+
+if(!$conn)
+{
+    die("<script>alert('Connection Failed.')</script>");
+}
+?>
